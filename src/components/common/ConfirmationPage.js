31770aac91a5e83a510d6f482c22e5791219c2a1
Saintmonza <lrmonza@gmail.com>
2021-10-05 17:41:59 +0100

    Components - Boilerplate Components Added (8 pages)

---
+++ src/components/common/ConfirmationPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const ConfirmationPage = () => {
+  return (
+    <div>
+      <h1>Confirmation Page</h1>
+    </div>
+  )
+}
+
+export default ConfirmationPage
---
+++ src/components/common/Register.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Registration Page</h1>
+    </div>
+  )
+}
+
+export default Register
