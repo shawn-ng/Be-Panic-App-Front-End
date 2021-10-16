ed16937d380adb7c94c7477d7868d3cbf4cb4d94
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2021-10-16 10:18:36 +0100

    new-changes

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -14,35 +14,37 @@ const ProductCard = ({
 }) => {
   return (
     <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile is-vcentered">
-      <Link to={`/product/${_id}`}>
-        <div className="card">
-          <div className="card-header has-background-light">
-            {/* <h4 className="card-header-title has-text-centered">{name}</h4> */}
-            <h4 className="card-header-title">
-              {name.length >= 24 ? name.slice(0, 24) + '...' : name}
-            </h4>
-          </div>
+      {/* <Link to={`/product/${_id}`}> */}
+      <div className="card">
+        <div className="card-header has-background-light">
+          {/* <h4 className="card-header-title has-text-centered">{name}</h4> */}
+          <h4 className="card-header-title">
+            {name.length >= 24 ? name.slice(0, 24) + '...' : name}
+          </h4>
         </div>
-        <div className="card-image">
-          <figure className="image is-1by1">
-            <img
-              src={imageUrl}
-              alt={name}
-              loading="lazy"
-              width="300"
-              height="300"
-            />
-          </figure>
-        </div>
-        <div className="notification link-focus-border has-background-light">
-          <h5>£{price}</h5>
+      </div>
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img
+            src={imageUrl}
+            alt={name}
+            loading="lazy"
+            width="300"
+            height="300"
+          />
+        </figure>
+      </div>
+      <div className="notification link-focus-border has-background-light">
+        <h5>£{price}</h5>
 
-          <h5>{stockCount} Left In Stock</h5>
-        </div>
-      </Link>
+        <h5>{stockCount} Left In Stock</h5>
+      </div>
+      {/* </Link> */}
       <button
+        type="button"
         className="button is-fullwidth mt-4 is-warning"
-        onClick={() =>
+        onClick={(e) => {
+          e.preventDefault()
           onItemSelect({
             _id,
             name,
@@ -54,7 +56,7 @@ const ProductCard = ({
             category,
             onItemSelect,
           })
-        }
+        }}
       >
         Buy
       </button>
